30a8bc10c09e13c38b92e4c5add377638cdcd905
unknown <ss5815190@gmail.com>
2023-03-20 19:24:40 +0800

    fix

--- src/pages/ScrollToTop.js
+++ src/pages/ScrollToTop.js
@@ -1,13 +1,34 @@
 import { useEffect } from "react";
+import { useLocation} from "react-router-dom";
 
 
+export default function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+/*
 const ScrollToTop = () => {
 
 useEffect(() => {
     window.history.scrollRestoration = 'manual'
+    console.log("manual")
   }, []);
 
   return null;
 };
 
 export default ScrollToTop;
+
+
+  const location = useLocation();
+  useEffect(() => {
+    if (!location.hash) {
+      window.scrollTo(0, 0);
+    }
+  }, [location]);
+*/
